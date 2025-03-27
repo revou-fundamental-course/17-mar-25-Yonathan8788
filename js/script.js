ca3c600c130ad2f7b96324860828dc5236494729
Yonathan8788 <yonataneward@gmail.com>
2025-03-27 20:24:30 +0700

    update konfig

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//ini javascript external
+/* Ini Java Script External */
+
+console.log("JavaScript is working!");
+
+// Form Validation
+function validateForm() {
+  const sisiInput = document.getElementById("sisi-input");
+
+  // Check if the input is empty
+  if (sisiInput.value == "") {
+    alert("Sisi harus diisi angka lebih besar dari 0!");
+  } else {
+    // Process the Calculation
+    console.log(sisiInput.value);
+  }
+}
